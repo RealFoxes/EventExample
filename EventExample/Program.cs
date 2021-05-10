bbb3e78be3f28af0f4fe482a9acb93a883d163a4
Real_Foxes <nikita0630panov@gmail.com>
2021-05-10 17:10:41 +0400

    Add project files.

---
+++ EventExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcessBusinessLogic bl = new ProcessBusinessLogic();
+			bl.MessageExecutor += MessageToUp; // register with an event
+			bl.MessageExecutor += MessageToReverse; // register with an event
+            bl.Message("1cGovno");
+            Console.ReadKey();
+        }
+
+        public static void MessageToReverse(string message)
+        {
+            Console.WriteLine(String.Join(" ", message.Reverse()));
+        }
+        public static void MessageToUp(string message)
+        {
+            Console.WriteLine(message.ToUpper());
+        }
+    }
+
+    public delegate void MessageExecutor(string message);  // delegate
+    public class ProcessBusinessLogic
+    {
+        public event MessageExecutor MessageExecutor; // event
+
+        public void Message(string message)
+		{
+            MessageExecutor?.Invoke(message);
+		}
+    }
+}
